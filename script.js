29f35050c5216a12b065f8ae94bb3ec030bd515f
Timmy Cho <chotimmysf@gmail.com>
2020-08-10 22:01:48 -0700

    valid email check, required fields check

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const close = document.getElementById("close");
 const open = document.getElementById("open");
 const modal = document.getElementById("modal");
 
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("confirm");
+const sec_question = document.getElementById("sec-question");
+const sec_answer = document.getElementById("sec-answer");
+
 // Toggle nav
 toggle.addEventListener("click", () =>
   document.body.classList.toggle("show-nav")
@@ -22,3 +29,49 @@ window.addEventListener("click", (e) =>
 // Shorthands: ? is "if" and : is "else"
 
 // Sign up validation
+
+//Check if email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check if passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([name, email, password, password2, sec_question, sec_answer]);
+  checkLength(name, 4, 28);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+  checkSecQuestion(sec_question, 5);
+  checkSecQuestion(sec_answer, 5);
+});
